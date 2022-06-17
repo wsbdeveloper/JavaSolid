547620d3ca6a554fd8a35d2a2d4f269144f81dc9
wsbengineer <wellington@up4tech.com.br>
2022-06-16 21:53:06 -0300

    feat: criando check de periodiciade de atualização de salario

--- src/br/com/alura/rh/service/ReajusteService.java
+++ src/br/com/alura/rh/service/ReajusteService.java
@@ -6,6 +6,7 @@ import br.com.alura.rh.model.Funcionario;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 public class ReajusteService {
     public void reajustarSalarioFuncionario(Funcionario fun, BigDecimal aumento){
@@ -13,6 +14,15 @@ public class ReajusteService {
         BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
         if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
             throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
+        }
+
+        LocalDate dataUltimoReajuste = fun.getDataUltimoReajuste();
+        LocalDate dataAtual = LocalDate.now();
+
+        long mesesDesdeUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, dataAtual);
+
+        if (mesesDesdeUltimoReajuste < 6) {
+            throw new ValidacaoException("Intervalo deve ser maior que 6 meses para reajuste!");
         }
 
         BigDecimal salarioReajustado = salarioAtual.add(salarioAtual);
